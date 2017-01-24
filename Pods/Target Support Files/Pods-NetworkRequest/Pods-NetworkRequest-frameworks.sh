6f07c2383c80f510941225670eb696a321b6b0ec
Marton Kerekes <marton.kerekes@moonpig.com>
2017-01-24 17:19:00 +0000

    removed braintree for prerelease

--- Pods/Target Support Files/Pods-NetworkRequest/Pods-NetworkRequest-frameworks.sh
+++ Pods/Target Support Files/Pods-NetworkRequest/Pods-NetworkRequest-frameworks.sh
@@ -82,10 +82,3 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Braintree/Braintree.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Braintree/Braintree.framework"
-fi
